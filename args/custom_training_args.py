ae37461de3c84b096276bd5013b9d1b41ef175a6
eyeol <daniel4906@korea.ac.kr>
2024-10-02 02:16:41 +0000

    feat: CustomTrainingArguments 기능 추가
    체크포인트 간격을 조절할 수 있는 save_steps를 추가 구현하였습니다

--- args/custom_training_args.py
+++ args/custom_training_args.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from typing import Optional
 from transformers import TrainingArguments
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple, Union
 
 
 # batch size와 epoch 및 다른 hyper parameter를 조정할 수 있습니다
@@ -10,5 +11,17 @@ class CustomTrainingArguments(TrainingArguments):
         default=8, metadata={"help": "Total number of training epochs to perform."}
     )
     num_train_epochs: float = field(
-        default=2, metadata={"help": "Total number of training epochs to perform."}
+        default=3, metadata={"help": "Total number of training epochs to perform."}
+    )
+    save_steps: int = field(
+        default=300, metadata={"help": "Save checkpoint every X updates steps."}
+    )
+    save_total_limit: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": (
+                "Limit the total amount of checkpoints. "
+                "Deletes the older checkpoints in the output_dir. Default is unlimited checkpoints"
+            )
+        },
     )
