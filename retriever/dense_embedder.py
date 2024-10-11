670f550eb18811b833022c94a9a186ae6ab87db0
eyeol <daniel4906@korea.ac.kr>
2024-10-11 07:56:04 +0000

    fix: 클래스 이름 변경 BertEncoder >> BertEmbedder

--- retriever/dense_embedder.py
+++ retriever/dense_embedder.py
@@ -5,9 +5,9 @@ from transformers import (
 
 
 # BertEncoder를 직접 수정하지 말고, 상속 받아서 쓰세요
-class BertEncoder(BertPreTrainedModel):
+class BertEmbedder(BertPreTrainedModel):
     def __init__(self, config):
-        super(BertEncoder, self).__init__(config)
+        super(BertEmbedder, self).__init__(config)
 
         self.bert = BertModel(config)
         self.init_weights()
