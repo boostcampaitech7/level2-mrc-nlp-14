5339b21947303a8a014a91ef6d4789aa7e9eb74f
eyeol <daniel4906@korea.ac.kr>
2024-10-14 08:30:28 +0000

    feat: dense retriever 세부 함수 구현 3
    임베딩 / 유사도 구하기 / retrieve까지 구현
    정확도가 매우 떨어진다는 문제가 있는데 원인 분석이 안되어있음
    일단 올려두고 train 부분 구현 한 다음에 다시 확인해야 한다

--- retriever/dense/dense_retriever.py
+++ retriever/dense/dense_retriever.py
@@ -20,26 +20,7 @@ from .dense_embedder import BertEmbedder
 
 class DenseRetriever(BaseRetriever):
     def __init__(self, args: DenseRetrieverArguments) -> NoReturn:
-        """
-        Arguments:
-            tokenize_fn:
-                기본 text를 tokenize해주는 함수입니다.
-                아래와 같은 함수들을 사용할 수 있습니다.
-                - lambda x: x.split(' ')
-                - Huggingface Tokenizer
-                - konlpy.tag의 Mecab
 
-            data_path:
-                데이터가 보관되어 있는 경로입니다.
-
-            context_path:
-                Passage들이 묶여있는 파일명입니다.
-
-            data_path/context_path가 존재해야합니다.
-
-        Summary:
-            Passage 파일을 불러오고 TfidfVectorizer를 선언하는 기능을 합니다.
-        """
         # BaseRetriever의 생성자를 호출하여 data_path를 초기화
         super().__init__(args.data_path)
 
