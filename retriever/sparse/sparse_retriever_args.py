bd443e75fcfc8b8c4bf23d930ca16b20b1377021
jagaldol <jagaldol.dev@gmail.com>
2024-10-14 09:09:50 +0000

    fix: dataclass 제거 및 생성자 하나로 간결화 진행

--- retriever/sparse/sparse_retriever_args.py
+++ retriever/sparse/sparse_retriever_args.py
@@ -1,24 +1,14 @@
-from dataclasses import dataclass
 from transformers import AutoTokenizer
 from args import RetrieverArguments
 
 
-@dataclass
 class SparseRetrieverArguments:
-    """
-    DenseRetriever 클래스를 생성할 때 전달할 인자값들
-    """
-
-    # Retrieve할 데이터 경로
-    data_path = "./data/"
-    context_path = "wikipedia_documents.json"
-
-    # 임베딩 종류 설정 (tfidf, count, hash, bm25)
-    embedding_type = "bm25"
-
-    tokenizer_name: str = "klue/bert-base"
-
     def __init__(self, retriever_args: RetrieverArguments):
+        # Retrieve할 데이터 경로
+        self.data_path = "./data/"
+        self.context_path = "wikipedia_documents.json"
+
+        # 임베딩 종류 설정 (tfidf, count, hash, bm25)
         self.embedding_type = retriever_args.sparse_embedding_type
         self.tokenizer_name = retriever_args.sparse_tokenizer_name
 
