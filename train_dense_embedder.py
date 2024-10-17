56fe18fc1b72a9e18202d6f3666fa352e78391ea
eyeol <daniel4906@korea.ac.kr>
2024-10-17 02:14:04 +0000

    fix: HfArgumentParser 사용하는 코드 추가
    - 데이터셋 경로를 하드코딩하지 않고 data_args에서 받아오기 위한 parsing 부분을 추가했습니다
    
    - 기존 args 역할은 dense_args가 하게 됩니다

--- train_dense_embedder.py
+++ train_dense_embedder.py
@@ -3,36 +3,39 @@ import torch.nn.functional as F
 from tqdm.auto import tqdm
 from datasets import load_from_disk
 import os
-from transformers import AutoTokenizer, set_seed
-from args import RetrieverArguments
+from transformers import HfArgumentParser, AutoTokenizer, set_seed
+from args import RetrieverArguments, DataTrainingArguments
 from retriever.dense.dense_embedder import BertEmbedder  # BertEmbedder 가져오기
 from retriever.dense.dense_retriever_args import (
     DenseRetrieverArguments,
 )  # Arguments 가져오기
-from transformers import set_seed
 
 
 # 학습 함수 정의
-def train_embedder(args: DenseRetrieverArguments):
+def train_embedder(
+    data_args: DataTrainingArguments, retriever_args: RetrieverArguments
+):
+    # dense_args 초기화
+    dense_args = DenseRetrieverArguments(retriever_args=retriever_args)
 
     # MRC 데이터셋 불러오기
-    train_data = load_from_disk("./data/train_dataset")["train"]
+    train_data = load_from_disk(data_args.dataset_name)["train"]
 
     # question과 context 준비
     questions = [item["question"] for item in train_data]
     contexts = [item["context"] for item in train_data]
 
     # p_encoder, q_encoder 초기화
-    if args.use_siamese:
-        encoder = BertEmbedder.from_pretrained(args.p_embedder_name)
+    if dense_args.use_siamese:
+        encoder = BertEmbedder.from_pretrained(dense_args.p_embedder_name)
         p_encoder = encoder
         q_encoder = encoder  # 동일한 인코더를 사용
     else:
-        p_encoder = BertEmbedder.from_pretrained(args.p_embedder_name)
-        q_encoder = BertEmbedder.from_pretrained(args.q_embedder_name)
+        p_encoder = BertEmbedder.from_pretrained(dense_args.p_embedder_name)
+        q_encoder = BertEmbedder.from_pretrained(dense_args.q_embedder_name)
 
     # 토크나이저 초기화 (p_encoder 이름에 기반하여)
-    tokenizer = AutoTokenizer.from_pretrained(args.p_embedder_name)
+    tokenizer = AutoTokenizer.from_pretrained(dense_args.p_embedder_name)
 
     # GPU로 옮기기
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -99,15 +102,19 @@ def train_embedder(args: DenseRetrieverArguments):
         )
 
     # 모델 저장
-    p_embedder_path = args.p_embedder_name.replace("/", "_")
-    q_embedder_path = args.q_embedder_name.replace("/", "_")
-    if args.use_siamese:
-        model_save_path = os.path.join(args.model_path, f"{p_embedder_path}_SDE")
+    p_embedder_path = dense_args.p_embedder_name.replace("/", "_")
+    q_embedder_path = dense_args.q_embedder_name.replace("/", "_")
+    if dense_args.use_siamese:
+        model_save_path = os.path.join(dense_args.model_path, f"{p_embedder_path}_SDE")
         p_encoder.save_pretrained(model_save_path)
         tokenizer.save_pretrained(model_save_path)
     else:
-        p_save_path = os.path.join(args.model_path, f"{p_embedder_path}_ADE_passage")
-        q_save_path = os.path.join(args.model_path, f"{q_embedder_path}_ADE_query")
+        p_save_path = os.path.join(
+            dense_args.model_path, f"{p_embedder_path}_ADE_passage"
+        )
+        q_save_path = os.path.join(
+            dense_args.model_path, f"{q_embedder_path}_ADE_query"
+        )
         p_encoder.save_pretrained(p_save_path)
         q_encoder.save_pretrained(q_save_path)
         tokenizer.save_pretrained(p_save_path)
@@ -119,10 +126,13 @@ def train_embedder(args: DenseRetrieverArguments):
 # main 함수
 if __name__ == "__main__":
     # DenseRetrieverArguments를 불러오고 필요 시 인자 설정
-    args = DenseRetrieverArguments(retriever_args=RetrieverArguments)
+
+    parser = HfArgumentParser((DataTrainingArguments, RetrieverArguments))
+
+    data_args, retriever_args = parser.parse_args_into_dataclasses()
 
     # 시드 설정 (재현성을 위해)
     set_seed(42)
 
     # 학습 함수 실행
-    train_embedder(args)
+    train_embedder(data_args, retriever_args)
