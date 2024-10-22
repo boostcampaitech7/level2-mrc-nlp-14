1a3e9a6054778630489f576c6af9a8a637147c9a
jagaldol <jagaldol.dev@gmail.com>
2024-10-22 11:38:32 +0000

    fix: metrics 함수들 코드 및 연산 최적화

--- retriever/metrics/retriever_metrics.py
+++ retriever/metrics/retriever_metrics.py
@@ -27,33 +27,27 @@ class RetrieverMetrics:
         return result
 
     def recall_at_k(self, k: int) -> float:
-        total_relevant_docs = 0
         relevant_retrieved = 0
         for _, row in self.df.iterrows():
             retrieved_docs = row[self.retrieved_documents_label][:k]
             relevant_doc = row[self.original_document_label]
-            relevant_retrieved += sum(
-                [1 for doc in retrieved_docs if doc == relevant_doc]
-            )
-            total_relevant_docs += 1
+            if relevant_doc in retrieved_docs:
+                relevant_retrieved += 1
 
-        recall = relevant_retrieved / total_relevant_docs
+        recall = relevant_retrieved / len(self.df)
         return recall
 
     def mean_reciprocal_rank(self) -> float:
-        reciprocal_ranks = []
+        reciprocal_ranks_sum = 0
         for _, row in self.df.iterrows():
             retrieved_docs = row[self.retrieved_documents_label]
             relevant_doc = row[self.original_document_label]
 
             # 정답 문서가 검색 결과에서 몇 번째에 있는지 찾기
-            if relevant_doc in retrieved_docs:
-                rank = (
-                    retrieved_docs.index(relevant_doc) + 1
-                )  # 인덱스는 0부터 시작하므로 +1
-                reciprocal_ranks.append(1 / rank)
-            else:
-                reciprocal_ranks.append(0)
+            for rank, doc in enumerate(retrieved_docs, start=1):
+                if doc == relevant_doc:
+                    reciprocal_ranks_sum += 1 / rank
+                    break
 
-        mrr = sum(reciprocal_ranks) / len(reciprocal_ranks)
+        mrr = reciprocal_ranks_sum / len(self.df)
         return mrr
