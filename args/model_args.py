2243ccd6be9c21616aa9890e317f70caeb1f094b
kimhyeonseo0830 <gustj3008@naver.com>
2024-10-22 03:01:11 +0000

    커스텀 손실 함수 확인

--- args/model_args.py
+++ args/model_args.py
@@ -25,3 +25,16 @@ class ModelArguments:
             "help": "Pretrained tokenizer name or path if not the same as model_name"
         },
     )
+    use_custom_loss: bool = field(
+        default=False,
+        metadata={
+            "help": "Whether to use QA attention loss for training"
+            }
+    )
+    custom_loss_weight: float = field(
+        default=0.1,
+        metadata={
+            "help": "If you are using a custom loss, this parameter assigns a weight to the custom loss component "
+            "in the total loss function, controlling its influence on model training."
+            }
+    )
