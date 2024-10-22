ca0fc183673dc56e745d07ea9b68f4adbca5defc
kimhyeonseo0830 <gustj3008@naver.com>
2024-10-22 13:25:23 +0000

    load_data_custom 함수 제거&feature를 생성자로 가져옴

--- additionaldata/similar_dataset.py
+++ additionaldata/similar_dataset.py
@@ -15,22 +15,21 @@ import os
 class UseSimilarData:
     def __init__(self, data_args: DataTrainingArguments):
         self.data_args = data_args
-
-    def load_dataset_custom(self, dataset_name: str) -> Dataset:
-        try:
-            if os.path.isdir(dataset_name):
-                print(f"로컬 디렉토리에서 데이터셋을 로드합니다: {dataset_name}")
-                dataset = load_from_disk(dataset_name)
-            else:
-                print(f"Hugging Face Hub에서 데이터셋을 로드합니다: {dataset_name}")
-                dataset = load_dataset(dataset_name, split="train")
-            return dataset
-        except Exception as e:
-            print(f"데이터셋 로딩 중 오류 발생: {e}")
-            raise
+        self.features = Features(
+            {
+                "id": Value("string"),
+                "title": Value("string"),
+                "context": Value("string"),
+                "question": Value("string"),
+                "answers": {
+                    "text": Sequence(Value("string")),
+                    "answer_start": Sequence(Value("int64")),
+                },
+            }
+        )
 
     def get_processed_data(self) -> Dataset:
-        original_dataset = self.load_dataset_custom(self.data_args.dataset_name)
+        original_dataset = load_from_disk(self.data_args.dataset_name)
         for split in ["train", "validation"]:
             columns_to_remove_my = [
                 col
@@ -46,16 +45,17 @@ class UseSimilarData:
                 .map(
                     add_question_mark,
                 )
-                .cast(features)
+                .cast(self.features)
             )
 
         if self.data_args.use_sim_data and self.data_args.sim_dataset_names:
+            print("추가적인 데이터를 사용합니다.")
             similar_datasets = []
             for sim_name in self.data_args.sim_dataset_names:
-                sim_dataset = self.load_dataset_custom(sim_name)
+                sim_dataset = load_dataset(sim_name, split="train")
                 sim_dataset = sim_dataset.map(
                     add_question_mark,
-                ).cast(features)
+                ).cast(self.features)
                 similar_datasets.append(sim_dataset)
 
             if similar_datasets:
@@ -84,17 +84,3 @@ def add_question_mark(example):
     if not example["question"].endswith("?"):
         example["question"] += "?"
     return example
-
-
-features = Features(
-    {
-        "id": Value("string"),
-        "title": Value("string"),
-        "context": Value("string"),
-        "question": Value("string"),
-        "answers": {
-            "text": Sequence(Value("string")),
-            "answer_start": Sequence(Value("int64")),
-        },
-    }
-)
