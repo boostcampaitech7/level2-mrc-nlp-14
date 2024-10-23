69f912e15b9a5dc5b008eac9aa7b9b7883259425
eyeol <daniel4906@korea.ac.kr>
2024-10-23 05:17:31 +0000

    refactor: df2datasets함수로 기능 분리

--- retriever/retriever.py
+++ retriever/retriever.py
@@ -9,7 +9,7 @@ from datasets import (
 )
 from args import DataTrainingArguments, CustomTrainingArguments, RetrieverArguments
 from base import BaseRetriever
-
+import pandas as pd
 from sentence_transformers import CrossEncoder
 
 ### 하드코딩된 부분 ###
@@ -30,24 +30,7 @@ def create_retriever(retriever_args: RetrieverArguments) -> BaseRetriever:
         raise ValueError(f"Invalid retriever type: {type}")
 
 
-def run_1stage_retrieval(
-    datasets: DatasetDict,
-    training_args: CustomTrainingArguments,
-    data_args: DataTrainingArguments,
-    retriever_args: RetrieverArguments,
-) -> DatasetDict:
-
-    # Query에 맞는 Passage들을 Retrieval 합니다.
-    retriever = create_retriever(retriever_args)
-
-    if data_args.use_faiss:
-        retriever.build_faiss(num_clusters=data_args.num_clusters)
-        df = retriever.retrieve_faiss(
-            datasets["validation"], topk=data_args.top_k_retrieval
-        )
-    else:
-        df = retriever.retrieve(datasets["validation"], topk=data_args.top_k_retrieval)
-
+def df2datasets(df: pd.DataFrame, training_args: CustomTrainingArguments):
     # test data 에 대해선 정답이 없으므로 id question context 로만 데이터셋이 구성됩니다.
     if training_args.do_predict:
         f = Features(
@@ -78,6 +61,27 @@ def run_1stage_retrieval(
         )
     datasets = DatasetDict({"validation": Dataset.from_pandas(df, features=f)})
     return datasets
+
+
+def run_1stage_retrieval(
+    datasets: DatasetDict,
+    training_args: CustomTrainingArguments,
+    data_args: DataTrainingArguments,
+    retriever_args: RetrieverArguments,
+) -> DatasetDict:
+
+    # Query에 맞는 Passage들을 Retrieval 합니다.
+    retriever = create_retriever(retriever_args)
+
+    if data_args.use_faiss:
+        retriever.build_faiss(num_clusters=data_args.num_clusters)
+        df = retriever.retrieve_faiss(
+            datasets["validation"], topk=data_args.top_k_retrieval
+        )
+    else:
+        df = retriever.retrieve(datasets["validation"], topk=data_args.top_k_retrieval)
+
+    return df2datasets(df, training_args=training_args)
 
 
 def cross_encoder_rerank(cross_encoder_model, queries, passages):
@@ -152,33 +156,4 @@ def run_2stage_retrieval(
     # df["context"]에 새로 묶인 passage들 저장
     df["context"] = selected_passages
 
-    # test data 에 대해선 정답이 없으므로 id question context 로만 데이터셋이 구성됩니다.
-    if training_args.do_predict:
-        f = Features(
-            {
-                "context": Value(dtype="string", id=None),
-                "id": Value(dtype="string", id=None),
-                "question": Value(dtype="string", id=None),
-            }
-        )
-    # train data 에 대해선 정답이 존재하므로 id question context answer 로 데이터셋이 구성됩니다.
-    elif training_args.do_eval:
-        df = df.drop(columns=["original_context"])
-        f = Features(
-            {
-                "answers": Sequence(
-                    feature={
-                        "text": Value(dtype="string", id=None),
-                        "answer_start": Value(dtype="int32", id=None),
-                    },
-                    length=-1,
-                    id=None,
-                ),
-                "context": Value(dtype="string", id=None),
-                "id": Value(dtype="string", id=None),
-                "question": Value(dtype="string", id=None),
-            }
-        )
-
-    datasets = DatasetDict({"validation": Dataset.from_pandas(df, features=f)})
-    return datasets
+    return df2datasets(df, training_args=training_args)
